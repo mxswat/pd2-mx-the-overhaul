169dcab8035ebbdbb23edf0862e62f997d6ead26
Massimo 'Mx' Cacciapaglia <massimocacciapaglia.13@gmail.com>
2021-05-19 16:00:21 +0100

    Cleaned up code

--- Hooks/addon_perks/upgradestweakdata.lua
+++ Hooks/addon_perks/upgradestweakdata.lua
@@ -1,22 +1,17 @@
 Hooks:PostHook(UpgradesTweakData, "init", "AddonPerk_UpgradesTweakData_init", function(self)
-	self:add_temporary("dodgeopath_speed", 0, 3)
-	self:add_temporary("dodgeopath_dodge", 0, 1)
-    self:add_temporary_def("dodgeopath_speed")
-    self:add_temporary_def("dodgeopath_dodge")
+	self:add_temporary_upgrades("dodgeopath_speed", 0, 3)
+	self:add_temporary_upgrades("dodgeopath_dodge", 0, 1)
 end)
 
-function UpgradesTweakData:add_temporary(name_id, generic_val, duration)
-	self.values.temporary[name_id] = {
+function UpgradesTweakData:add_temporary_upgrades(name_id, generic_val, duration)
+    self.values.temporary[name_id] = {
 		{
 			generic_val,
 			duration,
 		}
 	}
-	-- mx_print_table(self.values.temporary[name_id])
-end
 
-function UpgradesTweakData:add_temporary_def(name_id)
-	self.definitions["temporary_"..name_id.."_1"] = {
+    self.definitions["temporary_"..name_id.."_1"] = {
 		name_id = "menu_temporary_"..name_id.."_1",
 		category = "temporary",
 		upgrade = {
@@ -26,4 +21,3 @@ function UpgradesTweakData:add_temporary_def(name_id)
 		}
 	}
 end
-
--- Hooks/perk_throwables/upgradestweakdata.lua
+++ Hooks/perk_throwables/upgradestweakdata.lua
@@ -1,49 +1,34 @@
 Hooks:PostHook(UpgradesTweakData, "init", "VPPP_UpgradesTweakData_init", function(self)
-	self:throwable_values("yakuza_injector", 1, 8)
-	self:throwable_values("burglar_luck", 1, 8)
-	self:throwable_values("med_x", 1, 8)
-	self:throwable_values("adrenaline_shot", 2, 10)
-	self:throwable_values("spare_armor_plate", 1, 0.5)
-	self:throwable_values("liquid_armor", 2, 10)
-	self:throwable_values("blood_transfusion", 1, 0.5)
-	self:throwable_values("wick_mode", 1, 6)
-	self:throwable_values("emergency_requisition", 1, 2)
-	self:throwable_values("the_mixtape", 1, 4)
-	self:throwable_values("throwable_trip_mine", 1, 0.1)
-	self:throwable_values("jet", 1, 8)
-	self:throwable_values("whiff", 2, 8)
+	self:common_add_throwables("yakuza_injector", 1, 8)
+	self:common_add_throwables("burglar_luck", 1, 8)
+	self:common_add_throwables("med_x", 1, 8)
+	self:common_add_throwables("adrenaline_shot", 2, 10)
+	self:common_add_throwables("spare_armor_plate", 1, 0.5)
+	self:common_add_throwables("liquid_armor", 2, 10)
+	self:common_add_throwables("blood_transfusion", 1, 0.5)
+	self:common_add_throwables("wick_mode", 1, 6)
+	self:common_add_throwables("emergency_requisition", 1, 2)
+	self:common_add_throwables("the_mixtape", 1, 4)
+	self:common_add_throwables("throwable_trip_mine", 1, 0.1)
+	self:common_add_throwables("jet", 1, 8)
+	self:common_add_throwables("whiff", 2, 8)
 	-- self.values.player.body_armor.dodge[1] = 1 -- Debug always dodgde
-
-	self:common_add_throwable("yakuza_injector")
-	self:common_add_throwable("burglar_luck")
-	self:common_add_throwable("med_x")
-	self:common_add_throwable("auto_inject_super_stimpak")
-	self:common_add_throwable("adrenaline_shot")
-	self:common_add_throwable("spare_armor_plate")
-	self:common_add_throwable("liquid_armor")
-	self:common_add_throwable("blood_transfusion")
-	self:common_add_throwable("wick_mode")
-	self:common_add_throwable("emergency_requisition")
-	self:common_add_throwable("the_mixtape")
-	self:common_add_throwable("throwable_trip_mine")
-	self:common_add_throwable("jet")
-	self:common_add_throwable("whiff")
+	mx_print_table(self.specialization_descs)
 end)
+
+-- Todo make new override for [pd2-lua\lib\managers\menu\skilltreegui.lua]SpecializationTierItem:init the self._desc_string and use new macros
 
 --[[
 	cooldown_reduction on kill, does not includes the custom cooldown reductions, like on melee or explosive kill
 ]]
-function UpgradesTweakData:throwable_values(name_id, cooldown_reduction, duration)
+function UpgradesTweakData:common_add_throwables(name_id, cooldown_reduction, duration)
 	self.values.temporary[name_id] = {
 		{
 			cooldown_reduction,
 			duration,
 		}
 	}
-	-- mx_print_table(self.values.temporary[name_id])
-end
 
-function UpgradesTweakData:common_add_throwable(name_id)
 	self.definitions[name_id] = {
 		category = "grenade",
 	}
@@ -58,4 +43,3 @@ function UpgradesTweakData:common_add_throwable(name_id)
 		}
 	}
 end
-
