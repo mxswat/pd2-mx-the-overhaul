5860ac95acc77db02e3b90bdb3ae842f5b1a0783
Massimo 'Mx' Cacciapaglia <massimocacciapaglia.13@gmail.com>
2021-05-23 18:04:02 +0100

    Custom heat stakc health conversion

--- Hooks/skilltree_addon/SkillFramework/charactertweakdata.lua
+++ Hooks/skilltree_addon/SkillFramework/charactertweakdata.lua
@@ -1,7 +1,10 @@
+_G = _G or {}
+-- So, I had to make my own global, since I cant trush OVK to do shit properly, and I can't trust other modder to NOT FUCKING OVERRIDE EVERYTHING IN THEIR PATH. Rant over
+_G.ORIGINAL_HEALTH_MAP = {}
 Hooks:PostHook(CharacterTweakData, "init", "SF2_CharacterTweakData_init", function (self)
     for k, v in pairs(self) do
         if type(v) == "table" and v.HEALTH_INIT and v.weapon then
-            v.ORIGINAL_HEALTH_INIT = v.HEALTH_INIT
+            _G.ORIGINAL_HEALTH_MAP[k] = v.HEALTH_INIT
         end
     end
 end)
--- Hooks/skilltree_addon/pyromancer/playermanager.lua
+++ Hooks/skilltree_addon/pyromancer/playermanager.lua
@@ -1,10 +1,9 @@
 Hooks:PostHook(PlayerManager, "check_skills", "Pyromancer_check_skills", function(self)
 	if self:has_category_upgrade("flamethrower", "thermal_bomb") then
 		local function thermal_bomb(weapon_unit, variant, killed_unit)
-            local char_tweak = tweak_data.character[killed_unit:base()._tweak_table]
-            local increase = (char_tweak.ORIGINAL_HEALTH_INIT or 4) -- Fallback because overkill BS
+            local increase = (ORIGINAL_HEALTH_MAP[killed_unit:base()._tweak_table] or 4) -- Fallback because overkill BS
 			self._pyromancer_thermal_bomb = self._pyromancer_thermal_bomb + increase 
-            -- log(self._pyromancer_thermal_bomb.."/"..self:upgrade_value("flamethrower", "thermal_bomb").." +"..increase)
+            log(self._pyromancer_thermal_bomb.."/"..self:upgrade_value("flamethrower", "thermal_bomb").." +"..increase)
             if self._pyromancer_thermal_bomb > self:upgrade_value("flamethrower", "thermal_bomb") then
                 self._pyromancer_thermal_bomb = 0
                 -- Im aware this code is kinda bad, but it's the overkill way plus my fix for the fire 
