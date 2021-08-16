cf79981a60b3a4c23f82fd19d46c15fc5a9dcc14
mxswat <massimocacciapaglia.13@gmail.com>
2021-08-16 22:31:49 +0100

    Compatibilty patch with sydch mod

--- Hooks/addon_perks/skilltreetweakdata.lua
+++ Hooks/addon_perks/skilltreetweakdata.lua
@@ -1,56 +1,13 @@
 function AddAddonPerks(self)
-    local deck2 = {
-		cost = 0,
-		desc_id = "menu_deckall_2_desc",
-		name_id = "menu_deckall_2",
-		upgrades = {
-			"weapon_passive_headshot_damage_multiplier"
-		},
-		icon_xy = {
-			1,
-			0
-		}
-	}
-	local deck4 = {
-		cost = 0,
-		desc_id = "menu_deckall_4_desc",
-		name_id = "menu_deckall_4",
-		upgrades = {
-			"passive_player_xp_multiplier",
-			"player_passive_suspicion_bonus",
-			"player_passive_armor_movement_penalty_multiplier"
-		},
-		icon_xy = {
-			3,
-			0
-		}
-	}
-	local deck6 = {
-		cost = 0,
-		desc_id = "menu_deckall_6_desc",
-		name_id = "menu_deckall_6",
-		upgrades = {
-			"armor_kit",
-			"player_pick_up_ammo_multiplier"
-		},
-		icon_xy = {
-			5,
-			0
-		}
-	}
-	local deck8 = {
-		cost = 0,
-		desc_id = "menu_deckall_8_desc",
-		name_id = "menu_deckall_8",
-		upgrades = {
-			"weapon_passive_damage_multiplier",
-			"passive_doctor_bag_interaction_speed_multiplier"
-		},
-		icon_xy = {
-			7,
-			0
-		}
-	}
+    -- Updated to use the default perks from then current perkdecks, ence making it compatible with any existing overhaul mod (hopefully)
+    local deck2 = deep_clone(self.specializations[1][2])
+    deck2.cost = 0
+	local deck4 = deep_clone(self.specializations[1][4])
+    deck4.cost = 0
+	local deck6 = deep_clone(self.specializations[1][6])
+    deck6.cost = 0
+	local deck8 = deep_clone(self.specializations[1][8])
+    deck8.cost = 0
 
     local dodgeOpath = {
         {
