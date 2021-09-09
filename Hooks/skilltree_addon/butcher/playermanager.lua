51ba55fbdb7928fe48e01605dfc35057d918de6e
Massimo 'Mx' Cacciapaglia <massimocacciapaglia.13@gmail.com>
2021-09-10 00:11:09 +0200

    Improved wolverine stacks code

--- Hooks/skilltree_addon/butcher/playermanager.lua
+++ Hooks/skilltree_addon/butcher/playermanager.lua
@@ -7,18 +7,39 @@ function PlayerManager:update_wolverine_stacks(value)
 	self._decaying_stacks.wolverine_stacks = math.min(math.max(newValue, 0), 1);
 end
 
+PlayerManager.wolverineStackIncrease = 0.10
+PlayerManager.wolverineStackDecrease = 0.10 -- not used with wolverine
+PlayerManager.wolverineStackDecayDecrease = -0.05
+PlayerManager.wolverineStackDecayInterval = 2.5 -- seconds between one decay and the other
+
 Hooks:PostHook(PlayerManager, "_setup", "Butcher_PlayerManager__setup", function(self)
 	self._decaying_stacks = self._decaying_stacks or {}
 	self._decaying_stacks.wolverine_stacks = 0
+    PlayerManager.wolverineStackDecayInterval = self:upgrade_value("player", "butcher_melee_stacking") or 2.5
 
     if self:has_category_upgrade("player", "butcher_melee_stacking") then
 		local function increase_wolverine_stacks(damage_info)
             local attacker_unit = damage_info.attacker_unit
             local variant = damage_info.variant
-            mx_log_chat('variant', variant)
-			if variant == "melee" and attacker_unit == self:player_unit() then
+            local was_killed = damage_info.result.type == "death"
+            local is_saw = damage_info and damage_info.weapon_unit and damage_info.weapon_unit:base():is_category("saw")
+            if variant == "melee" or is_saw and attacker_unit == self:player_unit() then
 				self:update_wolverine_stacks(PlayerManager.wolverineStackIncrease)
+                if not was_killed then
+                    -- Since I already know that I hit someone in melee I might as well calculate and apply the extra damage now
+                    -- Using the mx_damage variant so I dont re-trigger this function
+                    -- local damage_mult = self:get_wolverine_stacks()
+                    local damage = damage_info.damage * self:get_wolverine_stacks()
+                    damage_info._unit:character_damage():damage_simple({
+                        variant = "mx_damage",
+                        damage = damage,
+                        attacker_unit = self:player_unit(),
+                        pos = damage_info.pos,
+                        attack_dir = Vector3(0, 0, 0)
+                    })
+                end
 			end
+
 		end
 
         CopDamage.register_listener("wolverine_stacks", {"SF2_on_damage"}, increase_wolverine_stacks)
@@ -26,11 +47,6 @@ Hooks:PostHook(PlayerManager, "_setup", "Butcher_PlayerManager__setup", function
 		CopDamage.unregister_listener("wolverine_stacks")
 	end
 end)
-
-PlayerManager.wolverineStackIncrease = 0.10
-PlayerManager.wolverineStackDecrease = 0.10 -- not used with wolverine
-PlayerManager.wolverineStackDecayDecrease = -0.05
-PlayerManager.wolverineStackDecayInterval = 2.5 -- seconds between one decay and the other
 
 Hooks:PostHook(PlayerManager, "update", "Butcher_PlayerManager_update", function(self, t, dt)
 	local player = self:player_unit()
